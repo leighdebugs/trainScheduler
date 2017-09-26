7a0a1241ebbb668b184fa19d20dc0b3290ae002b
leighkristine <lkroxmob@gmail.com>
2017-09-25 21:43:53 -0700

    initial js setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,32 @@
 
   var database = firebase.database();
 
-  //setup fields for data input
+  //submit button captures user input
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    //set variables for user input
+    var trainName = $("#name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainTime = moment ($("#time-input").val().trim(), "hh:mm").subtract(1, "years");
+    var trainFreq = $("#frequency-input").val().trim();  
+
+    //create temporary object to hold user input
+    var newTrain = {
+      name: trainName,
+      dest: trainDest,
+      time: trainTime
+      freq: trainFreq
+    };
+
+    database.ref().push(newTrain);
+
+    //log to console for confirmation
+    console.log(newTrain.name);
+    console.log(newTrain.dest);
+    console.log(newTrain.time);
+    console.log(trainFreq.freq);
+  });
   
   //calculate when next train will arrive
   //prepend input to div
