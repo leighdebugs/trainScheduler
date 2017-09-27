78bc07c47cd481579303c3cc3d0c12129d55ca95
leighkristine <lkroxmob@gmail.com>
2017-09-27 11:37:29 -0700

    added Firebase event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,78 @@
 //set up firebase
 
-  var config = {
-    apiKey: "AIzaSyCIq6iTzUQpZ0y_e8raGRu2gfERuXRZSMk",
-    authDomain: "trainscheduler-a6b38.firebaseapp.com",
-    databaseURL: "https://trainscheduler-a6b38.firebaseio.com",
-    projectId: "trainscheduler-a6b38",
-    storageBucket: "",
-    messagingSenderId: "372305213618"
+var config = {
+  apiKey: "AIzaSyCIq6iTzUQpZ0y_e8raGRu2gfERuXRZSMk",
+  authDomain: "trainscheduler-a6b38.firebaseapp.com",
+  databaseURL: "https://trainscheduler-a6b38.firebaseio.com",
+  projectId: "trainscheduler-a6b38",
+  storageBucket: "trainscheduler-a6b38.appspot.com",
+  messagingSenderId: "372305213618"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//submit button captures user input
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  //set variables for user input
+  var trainName = $("#name-input").val().trim();
+  var trainDest = $("#destination-input").val().trim();
+  var trainTime = $("#time-input").val().trim();
+  var trainFreq = $("#frequency-input").val().trim();  
+
+  //create temporary object to hold user input
+  var newTrain = {
+    name: trainName,
+    dest: trainDest,
+    time: trainTime,
+    freq: trainFreq,
   };
-  firebase.initializeApp(config);
 
-  var database = firebase.database();
+  database.ref().push(newTrain);
 
-  //submit button captures user input
-  $("#add-train-btn").on("click", function(event) {
-    event.preventDefault();
+  //log to console for confirmation
+  console.log(newTrain.name);
+  console.log(newTrain.dest);
+  console.log(newTrain.time);
+  console.log(newTrain.freq);
 
-    //set variables for user input
-    var trainName = $("#name-input").val().trim();
-    var trainDest = $("#destination-input").val().trim();
-    var trainTime = moment ($("#time-input").val().trim(), "hh:mm").subtract(1, "years");
-    var trainFreq = $("#frequency-input").val().trim();  
+  //clear text-boxes
+  $("#name-input").val("");
+  $("#destination-input").val("");
+  $("#time-input").val("");
+  $("#frequency-input").val("");
 
-    //create temporary object to hold user input
-    var newTrain = {
-      name: trainName,
-      dest: trainDest,
-      time: trainTime
-      freq: trainFreq
-    };
+});
 
-    database.ref().push(newTrain);
+//Firebase event for adding new train to database and HTML
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-    //log to console for confirmation
-    console.log(newTrain.name);
-    console.log(newTrain.dest);
-    console.log(newTrain.time);
-    console.log(trainFreq.freq);
-  });
-  
-  //calculate when next train will arrive
-  //prepend input to div
+  //store values in variables
+  var trainName = childSnapshot.val().name
+  var trainDest = childSnapshot.val().dest
+  var trainTime = childSnapshot.val().time
+  var trainFreq = childSnapshot.val().freq
+
+  //log to console for confirmation
+  console.log(trainName);
+  console.log(trainDest);
+  console.log(trainTime);
+  console.log(trainFreq);
+
+  //convert trainTime via momentjs
+  var timeUnix = moment.unix(trainTime).format("HH:mm");
+  console.log(timeUnix);
+
+  //calculate next arrival
+  var newFreq = trainFreq;
+  var firstTime = timeUnix;
+  var firstTimeConv = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConv);
+
+
+});
+
+
